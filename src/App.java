392432f7cbe4f78daa3390aa54931013dd5632cb
osaeid rony <osaeidfire@gmail.com>
2025-07-09 19:16:31 +0100

    pallindrome

--- src/App.java
+++ src/App.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 
 public class App {
     public static  String s="";
-        	public static void main(String[] args) {
+            public static void  Ispal(String word) {
+                
+                String reversed = new StringBuilder(word).reverse().toString();
+                System.out.println(reversed);
+                
+                if(word.equals(reversed)){
+                    System.out.println("this is a pallindrome");
+                }
+                else{
+                    System.out.println("this is not a pallindrome");
+                }
 
-                user_input();
 
 
+
+            }
+        	public static void main(String[] args, String string) {
+                Scanner s = new Scanner(System.in);
+                System.out.println("enter a word");
+                String word = s.nextLine();
+                Ispal(word);
+
+                
+                
             }
 }
 
