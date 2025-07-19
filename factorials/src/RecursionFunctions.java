b908a5420f0a9282390902ab7e6c5ebd5e3aab19
osaeid rony <osaeidfire@gmail.com>
2025-07-19 17:21:21 +0100

    rthrth

--- factorials/src/RecursionFunctions.java
+++ factorials/src/RecursionFunctions.java
@@ -20,6 +20,15 @@ public class RecursionFunctions {
             return Fibonacci(n - 1) + Fibonacci(n-2);
         }
     }
+
+    public static int Count(int d){
+        if(d <= 1){
+             return 1;
+         }
+         else{
+             return d + Count(d - 1);
+         }
+    }
     
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
@@ -32,5 +41,13 @@ public class RecursionFunctions {
         count = s.nextInt();
         int d = Fibonacci(count);
         System.out.println("the answer is," + d + ".");
+
+        System.out.println("Enter a number to get the count of.");
+        count = s.nextInt();
+        int f = Count(count);
+        System.out.println("the answer is," + f + ".");
+
     }
 }
+
+// 4 3 2 1
